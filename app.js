c70e2522e047049a9c263b5b9a29d077d6efa576
patty8909 <patty90_30@hotmail.com>
2017-10-10 12:22:18 -0500

    subiendo ejercicios

--- app.js
+++ app.js
@@ -3,10 +3,13 @@ function cipher (str){
     for (var i = 0; i < str.length; i++){
         var ascii = str[i].charCodeAt();
         if (ascii >= 65 && ascii <= 90){
-            solved += String.fromCharCode ((ascii+33)%26)
+            solved += String.fromCharCode ((ascii-65+33)%26)+65)
         }
-        else {
-            solved == str[i]
+        else if(ascii >= 97 && ascii <= 122) {
+            solved += String.fromCharCode ((ascii-97+33)%26)+97)
         }
     }
+    return solved
 }
+
+cipher ("ABCD")
